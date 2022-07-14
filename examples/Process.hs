516ff68b921e52777d145c3acfc21e85223ea5e6
Julian Ospald <hasufell@posteo.de>
2022-07-15 00:13:02 +0200

    Make Process example more useful

--- examples/Process.hs
+++ examples/Process.hs
@@ -1,22 +1,37 @@
 {-# LANGUAGE QuasiQuotes #-}
 
+import           Control.Exception (displayException)
 import           Control.Monad
+import           GHC.IO.Encoding (char8)
 import           System.OsPath
+import           System.OsPath.Encoding
 import           System.OsString ( osstr )
+import qualified System.OsString as OS
 import           System.Process
 
 
 main :: IO ()
 main = do
-  callProcess' [osp|ls|] [ [osstr|-la|] ]
+  stdout <- readProcess' [osp|ls|] [ [osstr|-la|] ] [osstr|.|]
+  print stdout
 
 
--- an emulation of System.Process.callProcess that uses OsPath
-callProcess' :: OsPath -> [OsString] -> IO ()
-callProcess' fp args = do
+-- an emulation of System.Process.readProcess that uses OsPath
+--
+-- ...we don't really need to use OsString for the result type
+readProcess' :: OsPath -> [OsString] -> OsString -> IO OsString
+readProcess' fp args stdin = do
   -- mimic base library behavior of dealing with FilePaths
-  fp' <- decodeFS fp
+  -- we go from OsPath to FilePath here
+  fp'   <- decodeFS fp
   args' <- forM args decodeFS
 
-  -- call String based process function
-  callProcess fp' args'
+  -- use char8 on unix and UCS-2 on windows, which should maintain the raw bytes
+  -- and should never fail
+  stdin' <- either (fail . displayException) pure $ OS.decodeWith char8 ucs2le stdin
+
+  -- run the process function
+  res <- readProcess fp' args' stdin'
+  -- convert from String to OsString ...let's pick the safest default: Utf-8 on unix
+  -- and UTF-16LE on windows
+  OS.encodeUtf res
